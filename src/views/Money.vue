da60ae194f9fc870ffac510c06bfc7fb1b18107c
磊哥 <493114008@qq.com>
2021-07-09 17:39:09 +0800

    初始化3个页面

---
+++ src/views/Money.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Money组件
+    </div>
+</template>
+
+<script lang='ts'>
+
+    export default  {
+        name:'Money'
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
